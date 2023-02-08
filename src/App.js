8e65d3265bbc7d5db59f4bffe1199f6d5efab6e7
Varsha-R <varsha@thediamondsolutions.com>
2023-02-08 15:52:10 -0800

    Removed comments

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ function App() {
   return (
     <PickerOverlay
       apikey={API_KEY}
-      // onSuccess={() => {
-      //   console.log("On Success");
-      // }}
-      // onUploadDone={(res) => {
-      //   console.log("On upload done");
-      //   console.log(res);
-      // }}
       pickerOptions={{
-        // accept: "image/*",
-        // fromSources: ["local_file_system", "googlephotos", "googledrive", "facebook"],
         fromSources: ["googlephotos", "googledrive"],
       }}
     />
